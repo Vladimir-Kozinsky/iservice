263fa1dbdc8cdc2bedeb7cc168c324a5796e4b45
Uladzimir Kazinski <kozinskytxc@gmail.com>
2023-08-09 20:40:38 +0200

    fix: hoc massages

--- front/src/HOC/wirhSuccessMessage.tsx
+++ front/src/HOC/wirhSuccessMessage.tsx
@@ -1,14 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import SuccessMessage from '../common/messages/SuccessMessage/SuccessMessage';
 import { RootState } from '../store/store';
 import { clearSuccessMessage } from '../store/reducers/aircraftReducer/aircraftReducer';
+import { clearLegSuccessMessage } from '../store/reducers/legReducer/legReducer';
 
 const withSuccessMessage = (Component: React.FC) => ({ ...props }) => {
     const aircraftSuccessMessage = useSelector((state: RootState) => state.aircraft.successMessage);
+    const legSuccessMessage = useSelector((state: RootState) => state.leg.successMessage);
+
     const NewComponent = () => {
         return (
             <>
                 {aircraftSuccessMessage && <SuccessMessage handler={clearSuccessMessage} message={aircraftSuccessMessage} />}
+                {legSuccessMessage && <SuccessMessage handler={clearLegSuccessMessage} message={legSuccessMessage} />}
                 <Component {...props} />
             </>)
 
