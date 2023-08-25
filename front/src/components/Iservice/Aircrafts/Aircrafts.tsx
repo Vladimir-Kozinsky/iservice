286dcd4db5747688c25d3e2e1f5f8ef5bf65457a
Uladzimir Kazinski <kozinskytxc@gmail.com>
2023-08-25 16:45:42 +0200

    refact: aircraft file

--- front/src/components/Iservice/Aircrafts/Aircrafts.tsx
+++ front/src/components/Iservice/Aircrafts/Aircrafts.tsx
@@ -6,23 +6,20 @@ import { AppDispatch, RootState } from "../../../store/store";
 import { getAircrafts } from "../../../store/reducers/aircraftReducer/aircraftReducer";
 import { IAircraft } from "../../../types/types";
 import AircraftWidget from "../Dashboard/AircraftWidget/AircraftWidget";
-import { useNavigate } from "react-router-dom";
 
 const Aircrafts: React.FC = () => {
     const dispatch = useDispatch<AppDispatch>();
     const aircraftsArr = useSelector((state: RootState) => state.aircraft.aircafts);
 
-    const aircrafts = () => aircraftsArr.map((aircaft: IAircraft)=> (
-        <AircraftWidget key={aircaft._id} aircraft={aircaft} />
-    ))
-
     useEffect(() => {
-        dispatch(getAircrafts());
+       dispatch(getAircrafts())
     }, [])
 
     return (
         <div className={s.aircrafts} >
-            {aircrafts()}
+            {aircraftsArr.map((aircaft: IAircraft) => (
+                <AircraftWidget key={aircaft._id} aircraft={aircaft} />
+            ))}
             <EmptyWidget route={"new-aircraft"} />
         </div>
     )
