1b23a8a846d75b970c3f29c5edf8608c9656ffc4
Uladzimir Kazinski <kozinskytxc@gmail.com>
2023-09-03 08:37:43 +0200

    refact: file widget

--- front/src/common/FileWidget/FileWidget.tsx
+++ front/src/common/FileWidget/FileWidget.tsx
@@ -1,15 +1,23 @@
+import classNames from "classnames";
 import s from "./FileWidget.module.scss"
 
 type IFileWidgetProps = {
     text: string;
     img: string;
     handler?: () => void;
+    isMute?: boolean;
 }
 
-const FileWidget = ({ text, img, handler }: IFileWidgetProps) => {
+const FileWidget = ({ text, img, handler, isMute }: IFileWidgetProps) => {
+
+    const onClickHandler = () => {
+        if (!isMute && handler) handler()
+    }
+
+    const widgetStyle = classNames(s.widget, isMute&& s["mute"])
 
     return (
-        <div className={s.widget} onClick={handler} >
+        <div className={widgetStyle} onClick={onClickHandler} >
             <h3>{text}</h3>
             <img src={img} alt="icon" />
         </div>
