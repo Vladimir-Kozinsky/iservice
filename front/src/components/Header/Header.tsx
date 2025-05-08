b3e08b05351959131fcd2a7da621c032ba11e9c9
Uladzimir Kazinski <kozinskytxc@gmail.com>
2025-05-08 19:51:21 +0400

    fix: header

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -6,12 +6,15 @@ import logo from './../../assets/img/png/logo.png'
 import avatar from './../../assets/img/png/avatar.png';
 import { signOut } from '../../store/reducers/authReducer/authReducer';
 import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+import { useState } from 'react';
 
 type HeaderProps = {
     theme?: string
 }
 
 const Header = ({ theme }: HeaderProps) => {
+    const [isMenu, setIsMenu] = useState(false)
     const user = useSelector((state: any) => state.auth.user);
     const dispatch = useDispatch<AppDispatch>();
     const logout = () => {
@@ -25,23 +28,36 @@ const Header = ({ theme }: HeaderProps) => {
                     <img src={logo} alt='logo' />
                     <h2 className={s.header__logo__title} >I-Service</h2>
                 </div>
-                <div className={s.header__links}>
-                    <NavLink className={s.link} to="/">Store</NavLink>
-                    <NavLink className={s.link} to="tools">Tools</NavLink>
-                    {/* <NavLink className={s.link} to="requests">Requests</NavLink> */}
+
+                <div className={s.nav__wrapper} >
+                    <nav className={classNames(s.headerMain__nav, isMenu && s.active)}>
+                        <ul>
+                            <li><NavLink className={classNames(s.headerMain__nav__link, s.headerMain__nav__profile)} to="profile">Profile</NavLink></li>
+                            <li><NavLink className={s.headerMain__nav__link} to="/">Store</NavLink></li>
+                            <li><NavLink className={s.headerMain__nav__link} to="tools">Tools</NavLink></li>
+                            <li><NavLink className={s.headerMain__nav__link} to="requests">Requests</NavLink></li>
+                            <li> <Button text='Log Out' color={color} btnType='button' handler={logout} /></li>
+                        </ul>
+                    </nav>
+                    <div className={classNames(s.burger__icon, isMenu && s.active)} onClick={isMenu ? () => setIsMenu(false) : () => setIsMenu(true)} >
+                        <span className={classNames(s.burger__icon__line, s.first)} ></span>
+                        <span className={classNames(s.burger__icon__line, s.second)} ></span>
+                        <span className={classNames(s.burger__icon__line, s.third)} ></span>
+                    </div>
                 </div>
-                <div className={s.header__nav} >
+                <div className={s.profile__wrapper}>
                     <NavLink className={s.link} to="profile">
                         <div className={s.link__imgWrap}  >
                             <img src={avatar} alt="user-avatar" />
                         </div>
                         <span>{`${user.firstName} ${user.lastName}`}</span>
                     </NavLink>
-
                     <Button text='Log Out' color={color} btnType='button' handler={logout} />
-                </div>
-            </div>
 
+                </div>
+
+
+            </div>
         </div>
     )
 }
