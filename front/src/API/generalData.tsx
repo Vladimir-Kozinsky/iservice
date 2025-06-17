626de0b7ef382543899daf68f290e1a493bbcaf1
Uladzimir Kazinski <kozinskytxc@gmail.com>
2025-06-17 19:15:36 +0400

    fix cors

--- front/src/API/generalData.tsx
+++ front/src/API/generalData.tsx
@@ -4,9 +4,9 @@ interface IGeneralAPIData {
 
 const generalAPIData: IGeneralAPIData = {
  //baseURL: "http://localhost:5000"
- baseURL: "https://iservice-backend-production.up.railway.app/"
+ //baseURL: "https://iservice-backend-production.up.railway.app/"
   // baseURL: "https://i-service.onrender.com"
- //baseURL: "https://iservice-backend-34ce5ioc8-uladzimirs-projects-457d517a.vercel.app/"
+ baseURL: "https://iservice-backend-34ce5ioc8-uladzimirs-projects-457d517a.vercel.app/"
 }
 
 export default generalAPIData;
